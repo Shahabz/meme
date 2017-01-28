9ae1021938ceed8b8e4a9eb69344850c5ef2078e
watanabe.everystudio <watanabe.everystudio@gmail.com>
2017-01-28 22:41:51 +0900

    Asset:TileToolを追加 ダンジョンを追加

---
+++ Assets/TileTool/Scripts/Editor/TileToolTileEditor.js
@@ -0,0 +1,21 @@
+﻿@CustomEditor(TileToolTile)
+public class TileToolTileEditor extends Editor {	
+	var tex: Texture;
+	
+		function OnEnable(){
+			tex = Resources.Load("TT_SideHelp");
+		target.FindAndSetTileValues();
+		var TTS:TileToolStyle = target.gameObject.GetComponent(TileToolStyle);
+		if(!TTS){
+			target.gameObject.AddComponent(TileToolStyle);
+		}
+		if (GUI.changed){
+			EditorUtility.SetDirty(target);
+		}
+	}					
+    override function OnInspectorGUI () {
+    	DrawDefaultInspector();	
+		var titleStyle = new GUIStyle(GUI.skin.label);
+		GUILayout.Label(tex, titleStyle);
+	}	
+}
