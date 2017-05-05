a6cf1b9e7f8d243656f41b01bbff5895a2c2e24c
watanabe.everystudio <watanabe.everystudio@gmail.com>
2017-05-05 13:15:48 +0900

    リソース管理用にいろいろと調整

---
+++ Assets/10script/SkitConnector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkitConnector : Singleton<SkitConnector> {
+
+	[SerializeField]
+	private SkitRoot m_skitRoot;
+
+	public override void Initialize()
+	{
+		base.Initialize();
+		SetDontDestroy(false);
+	}
+
+	public SkitRoot root
+	{
+		get { return m_skitRoot; }
+	}
+}
