ee888465e65f762c35a565287d7ce8eb1ac42336
watanabe.everystudio <watanabe.everystudio@gmail.com>
2017-06-15 23:51:37 +0900

    ヘッダーとかの位置調整

---
+++ Assets/10script/UICommandDungeon/CommandDungeonMain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandDungeonMain : Singleton<CommandDungeonMain> {
+
+	public void OnClickRouteRight()
+	{
+		Debug.Log("OnClickRouteRight");
+
+	}
+	public void OnClickRouteLeft()
+	{
+		Debug.Log("OnClickRouteLeft");
+	}
+}
