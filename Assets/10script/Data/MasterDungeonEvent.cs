24a148e0e617efc63c1b6da14a247f296a1a0b38
watanabe.everystudio <watanabe.everystudio@gmail.com>
2017-06-20 08:11:16 +0900

    イベントとかを追加する準備

---
+++ Assets/10script/Data/MasterDungeonEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterDungeonEventParam : CsvDataParam
+{
+	public int dungeon_event_id { get; set; }
+	public string title { get; set; }
+
+	public string type { get; set; }
+
+	public int level { get; set; }
+	public string description { get; set; }
+
+}
+
+public class MasterDungeonEvent : CsvData<MasterDungeonEventParam> {
+
+
+}
---
+++ Assets/10script/UICommandDungeon/IconDungeonEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconDungeonEvent : MonoBehaviour {
+
+	[SerializeField]
+	private Image m_imgIcon;
+	[SerializeField]
+	private Text m_txtTitle;
+
+
+
+}
